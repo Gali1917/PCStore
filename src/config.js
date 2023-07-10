db7777045363a51f95bc6081b5cf9ae5b4ec04c0
Gali1917 <carlgale93@hotmail.com>
2023-07-10 13:28:53 -0500

    Add/Gali1917/Cliente a;adido

--- src/config.js
+++ src/config.js
@@ -3,3 +3,4 @@ import {config} from 'dotenv';
 config();
 
 export const MONGODB = process.env.MONGODB || '';
+export const PORT = process.env.PORT || 5005;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import './config/mongoose';
+import { PORT } from './config';
 import productRoutes from "./routes/products.routes";
 
 const app = express();
@@ -7,5 +8,5 @@ const app = express();
 app.use(express.json());
 app.use(productRoutes);
 
-app.listen(5005);
-console.log('Server on port', 5005);
+app.listen(PORT);
+console.log('Server on port', PORT);
